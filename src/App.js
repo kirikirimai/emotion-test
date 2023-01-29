9870a99d26c9087355e5556a5e743ccc5f779b17
kirikirimai <go.revolution.no3@gmail.com>
2023-01-29 22:23:08 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+/** @jsxImportSource @emotion/react */
+import { css, Global } from "@emotion/react";
+import { useState } from "react";
+import styled from "@emotion/styled";
+
+const underLine = css`
+  border-bottom: 1px solid #ff0000;
+  padding-bottom: 10px;
+  display: inline-block;
+`;
+
+const ttlh1 = css`
+  color: red;
+  font-size: 30px;
+  background-color: #ffff00;
+  ${underLine}
+  @media (max-width: 767px) {
+    font-size: 8vw;
+  }
+
+  &.is-clicked {
+    background-color: blue;
+  }
+`;
+
+const globalBody = css`
+  body {
+    margin: 0;
+    background: #cfcfcf;
+  }
+`;
+
+const FontColor = styled.p`
+  color:${props => (props.clicked ? "blue" : "red")};
+  font-size:20px;
+  font-weight:bold;
+
+`;
+
 
 function App() {
+  const [clicked, setClicked] = useState(false);
+
+  const clickHandler = () => {
+    setClicked(!clicked);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global styles={globalBody} />
+
+      <div className="app">
+        <h1 css={ttlh1}>
+          EmotoinCSSの練習
+        </h1>
+
+        <FontColor clicked={clicked} onClick={clickHandler}>Styled componentsのようにクリックして文字色を帰る</FontColor>
+      </div>
+    </>
   );
 }
 
