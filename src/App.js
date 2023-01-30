c4ec5eddd46445d5b0515607a7560d04346c4529
kirikirimai <go.revolution.no3@gmail.com>
2023-01-30 15:14:37 +0900

    add css

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ const ttlh1 = css`
     background-color: blue;
   }
 
-  &:hover{
-    color:blue;
+  &:hover {
+    color: blue;
   }
 `;
 
@@ -36,12 +36,51 @@ const globalBody = css`
 `;
 
 const FontColor = styled.p`
-  color:${props => (props.clicked ? "blue" : "red")};
-  font-size:20px;
-  font-weight:bold;
-
+  color: ${(props) => (props.clicked ? "blue" : "red")};
+  font-size: 20px;
+  font-weight: bold;
 `;
 
+const box = css`
+  width: 300px;
+  height: 300px;
+  margin: auto;
+`;
+const bgColorRed = css`
+  background-color: red;
+`;
+
+const rollOver = css`
+  font-weight: bold;
+  font-size: 20px;
+  &:hover {
+    opacity: 0.5;
+  }
+
+  &::before {
+    content: "●";
+    display: inline-block;
+  }
+
+  @media (max-width: 767px) {
+    font-size: 8vw;
+  }
+`;
+
+const rollOverObj = css({
+  fontWeight: "bold",
+  fontSize: "20px",
+  "&:hover": {
+    opacity: 0.5,
+  },
+  "&::before": {
+    content: '"●"',
+    display: "inline-block",
+  },
+  "@media (max-width: 767px)":{
+    fontSize: "8vw",
+  }
+});
 
 function App() {
   const [clicked, setClicked] = useState(false);
@@ -54,11 +93,22 @@ function App() {
       <Global styles={globalBody} />
 
       <div className="app">
-        <h1 css={ttlh1}>
-          EmotoinCSSの練習
-        </h1>
+        <h1 css={ttlh1}>EmotoinCSSの練習</h1>
+        <div css={[box, bgColorRed]}>ここはボックス</div>
+        <p
+          css={css`
+            font-size: 30px;
+            color: red;
+          `}
+        >
+          インライン
+        </p>
+        <p css={rollOver}>ロールオーバー</p>
+        <p css={rollOverObj}>ロールオーバーオブジェクト</p>
 
-        <FontColor clicked={clicked} onClick={clickHandler}>Styled componentsのようにクリックして文字色を帰る</FontColor>
+        <FontColor clicked={clicked} onClick={clickHandler}>
+          Styled componentsのようにクリックして文字色を帰る
+        </FontColor>
       </div>
     </>
   );
